ce5943503566e4c3835cbb7bcd634d5ea7b2cb65
Aleah Manzi <aleah.manzi@gmail.com>
2017-02-09 20:12:54 -0500

    ensuring project is up to date for session

--- authentication/Google_oauth.js
+++ authentication/Google_oauth.js
@@ -1,3 +1,7 @@
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -5,38 +9,62 @@ const jsonParser=require('body-parser').json();
 const mongoose = require('mongoose');
 const userInfo = mongoose.model('userInfo');
 
-const addressInfo = mongoose.model('addressInfo');
-
-
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
 const GOOGLE_CLIENT_ID = "374972509400-ruvdflor4ctv70d0gdqi28ufkcc5ac6k.apps.googleusercontent.com";
 const { GOOGLE_CLIENT_SECRET } = require('../key.json') || process.env;
 
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+router.use(passport.initialize());
+router.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://127.0.0.1:8080/auth/google/callback",
-    //passReqToCallback   : true
   },
+  
   function(accessToken, refreshToken, profile, done) {
+    console.log("Google", profile);
 
-       console.log(profile);
-       userInfo.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
+    userInfo.findOne({'google.id': profile.id}).exec()
+      .then(function (user) {
+
+        if (user) {
+          return user;
+        } else {
+          return userInfo.create({
+            name: profile.name.givenName,
+            email: profile.emails[0].value,
+            google: {
+              id: profile.id
+            }
+          });
+        }
+
+      })
+      .then(function (userToLogin) {
+        console.log("the newly created user", userToLogin)
+        console.log("accesstoken", accessToken)
+        done(null, userToLogin);
+      })
+      .catch(function (err) {
+        console.error('Error creating user', err);
+        done(err);
+      });
   }
 ));
 
-router.get('/',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+router.get('/', passport.authenticate('google', { 
+	scope: [
+	'https://www.googleapis.com/auth/userinfo.email'
+	]
+	}));
 
 
-router.get('/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
+router.get('/callback', passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
 
 module.exports = router;
+
+
