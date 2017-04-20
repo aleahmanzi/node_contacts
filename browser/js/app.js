27d49f0b03fba1f5a5faa78e1d9769ad6b7624b6
Aleah Manzi <aleah.manzi@gmail.com>
2017-04-20 19:55:46 -0400

    landing page & css

--- browser/js/app.js
+++ browser/js/app.js
@@ -3,17 +3,10 @@ var myApp = angular.module('myApp', ['ngRoute','ngAnimate', 'ui.bootstrap'])
 
 	angular.module('myApp').controller('CollapseDemoCtrl', function ($scope, $rootScope) {
 	  $scope.isNavCollapsed = true;
-	  $rootScope.login_msg = true;
-	  $rootScope.mini_nav = false
-	  $rootScope.addBtn = false;
+	  $rootScope.mini_nav = true;
+	  $rootScope.addBtn = true;
 	  $scope.loginData = $rootScope.userData;
 
-	  $scope.hideMsg = function(){
-	  	$rootScope.login_msg = false;
-	  	$rootScope.mini_nav = true;
-	  	$rootScope.addBtn = true;
-
-	  }
 
 
 
